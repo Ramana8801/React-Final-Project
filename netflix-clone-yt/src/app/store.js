91ef72c23cf8183f438813a9784306023ba10fbd
Ramana8801 <90092732+Ramana8801@users.noreply.github.com>
2021-11-04 01:00:49 +0530

    Update store.js

--- netflix-clone-yt/src/app/store.js
+++ netflix-clone-yt/src/app/store.js
@@ -1,9 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore,getDefaultMiddleware } from '@reduxjs/toolkit';
 import userReducer from '../features/userSlice';
+import createSagaMiddleware from 'redux-saga'
+import { applyMiddleware, } from 'redux';
+import saga from '../saga'
+const sagaMiddleware = createSagaMiddleware();
+
+  applyMiddleware(sagaMiddleware)
+  const middleware = [...getDefaultMiddleware({ thunk: false }), sagaMiddleware];
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     
   },
+  middleware
 });
+
+sagaMiddleware.run(saga);
